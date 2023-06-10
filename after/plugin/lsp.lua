1bc2070fa539283d7cd6b0175d421e25e5d97db8
007Nil <sagniksarkar@ymail.com>
2023-06-11 01:43:29 +0530

    Initial version :)

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,17 @@
+local lsp = require('lsp-zero').preset({
+    name = 'minimal',
+    set_lsp_keymaps = true,
+    manage_nvim_cmp = true,
+    suggest_lsp_servers = true,
+})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- When you don't have mason.nvim installed
+-- You'll need to list the servers installed in your system
+lsp.setup_servers({'tsserver', 'eslint','jedi_language_server','gopls'})
+
+
+lsp.setup()
